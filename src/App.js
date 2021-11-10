a10e350ef8ead0f1c045e0c8eac9f8cb5f32a543
manpreet singh <manpreet_jsb@hotmail.com>
2021-11-10 10:39:57 +0000

    project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div>
+        <div>
+          <div className="card w-75">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up the bulk of the cards
+                content.
+              </p>
+              <span>Card link</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Bottom Area */}
+        <div className="fixed-bottom bg-info">
+          <div className="container ">
+            <div className="input-group m-2">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Message"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <div className="input-group-append">
+                <button className="btn btn-outline-secondary" type="button">
+                  SEND
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
   );
 }
 
